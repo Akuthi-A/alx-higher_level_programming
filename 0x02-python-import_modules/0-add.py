9adedecf98cbd27e6114f100283f3d052c315e9d
Akuthi <akuthiapril99@gmail.com>
2023-08-11 06:24:28 -0700

    import modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -10,5 +10,3 @@ if __name__ == "__main__":
     b = 2
 
     print("{} + {} = {}".format(a, b, add(a, b)))
-   # print(f"{a} + {b} = {add(a, b)}")
-
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,13 +2,12 @@
 
 if __name__ == "__main__":
     import sys
-    
+
     sum = 0
     if (len(sys.argv) == 0):
-            sum = 0
+        sum = 0
     for arg in range(1, len(sys.argv)):
-        
+
         sum += int(sys.argv[arg])
 
     print(sum)
-
