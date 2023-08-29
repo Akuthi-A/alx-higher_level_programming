48c6705a7caca85386dac286f3797208632b447a
Akuthi <akuthiapril99@gmail.com>
2023-08-29 10:04:07 -0700

    classy oop

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+class Square:
+    """Blueprint of a square."""
+    pass
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+class Square:
+    """blueprint of a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        getter function for size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        setter function for size
+
+        Args:
+            value (int): input size
+        Raises:
+            TypeError: if size is not an int
+            ValueError: if size < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+    """Return the area of the square."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+    """Print the square with the # character."""
+    for i in range(0, self.__size):
+        [print("#", end="") for j in range(self.__size)]
+        print("")
+    if self.__size == 0:
+        print("")
