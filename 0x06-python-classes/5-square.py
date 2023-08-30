7d7dbb5fcdb23e5989272d62369546d7c0aab2b1
Akuthi <akuthiapril99@gmail.com>
2023-08-29 17:15:49 -0700

    square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -34,11 +34,11 @@ class Square:
             TypeError: if size is not an int
             ValueError: if size < 0
         """
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """Return the area of the square."""
