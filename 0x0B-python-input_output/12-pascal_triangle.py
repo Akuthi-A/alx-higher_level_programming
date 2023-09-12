fe4372d6422476ec3147551c3de81165a0593f5e
Akuthi <akuthiapril99@gmail.com>
2023-09-12 15:46:02 -0700

    Pascal Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+     returns a list of lists of integers
+     representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    pascal = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = pascal[-1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        pascal.append(row)
+
+    return pascal
