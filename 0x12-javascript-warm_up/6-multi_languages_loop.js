5de49b1f5ea159e632c9f64a52d1f60e8b9bd503
Akuthi <112417255+Akuthi-A@users.noreply.github.com>
2024-03-30 20:32:29 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const sentences = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
-for (let sentence in sentences)
-{
-	console.log(sentences[sentence]);
-}
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-function add(a, b)
-{
-	let sum = a + b;
-	console.log(sum);
-}
-
-add(Number(process.argv[2]), Number(process.argv[3]));
