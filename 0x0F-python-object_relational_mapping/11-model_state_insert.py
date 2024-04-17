cb9f302223b70db4cfcf062b3c724ad176badfd3
Akuthi <akuthiapril99@gmail.com>
2024-04-16 17:24:36 -0700

    added new State obj

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+lists all State objects from db hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    engine = engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+   # Base.metadata.create_all(engine)
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    states = session.query(State).filter_by(name="Louisiana").first()
+
+    print(states.id)
+
